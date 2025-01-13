66a705790b2184ca23970e0aa2edccc5c070e1ed
bigbosspirrat <bigbosspirrat@gmail.com>
2025-01-13 10:34:40 +0100

    Initial commit

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/app/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SELECT | Snowflake optimization & cost management platform",
+  description:
+    "Gain visibility into Snowflake usage, optimize performance and automate savings with the click of a button.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
